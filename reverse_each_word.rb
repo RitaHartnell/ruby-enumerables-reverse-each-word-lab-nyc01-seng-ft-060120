e9b6a9dd2936eb756acaeefaa6ae9a1d4c149c4a
Melody Soriano <mel.soriano6123@gmail.com>
2020-05-11 23:51:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,11 @@ def reverse_each_word(string)
   #turn the string into an array, then use collect on it to enumerate through the string
   array = string.split(' ')
   p array
-  array.collect {|word| 
+  array.collect {|word|
     word.reverse
   }
   p array
-  
+
 end
 
 myString = "Hello, how are you?"
